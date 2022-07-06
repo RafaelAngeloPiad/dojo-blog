bc9c830d48eb0789c1b4e3fa05218519a5ca533f
thisdotEric <siguenzajohneric@gmail.com>
2022-07-06 13:46:56 +0800

    React should be imported on top of every jsx file

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,22 +1,20 @@
-const BlogList = ({blogs,title}) => {
+import React from 'react';
 
-    //const blogs = props.blogs;
-    //const title = props.title;
+const BlogList = ({ blogs, title }) => {
+  //const blogs = props.blogs;
+  //const title = props.title;
 
-
-
-    return ( 
-        <div className="blog-list">
-            <h2>{title}</h2>
-            {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-        ))}
+  return (
+    <div className='blog-list'>
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
         </div>
+      ))}
+    </div>
+  );
+};
 
-     );
-}
- 
 export default BlogList;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,24 @@
+import React from 'react';
+
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>THE DOJO BLOG</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "black",
-                    borderRadius: "8px"
-                }}>New Blog</a>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className='navbar'>
+      <h1>THE DOJO BLOG</h1>
+      <div className='links'>
+        <a href='/'>Home</a>
+        <a
+          href='/create'
+          style={{
+            color: 'white',
+            backgroundColor: 'black',
+            borderRadius: '8px',
+          }}
+        >
+          New Blog
+        </a>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;

122d1e6479bb629ac4db9c179ec116ebf89a3444
RafaelAngeloPiad <watchingmaniac@gmail.com>
2022-07-06 13:36:00 +0800

    react practice

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>THE DOJO BLOG</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "black",
+                    borderRadius: "8px"
+                }}>New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
